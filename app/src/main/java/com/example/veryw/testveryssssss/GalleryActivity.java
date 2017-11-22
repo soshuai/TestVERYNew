39289c72b2bd8ee17e11219a3bf662d575c82261
soshuai <601647787@qq.com>
2017-11-22 10:06:15 +0800

    添加3d画廊

---
+++ app/src/main/java/com/example/veryw/testveryssssss/GalleryActivity.java
@@ -0,0 +1,157 @@
+package com.example.veryw.testveryssssss;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+import com.example.veryw.testveryssssss.base.BaseActivity;
+import com.example.veryw.testveryssssss.util.ImageUtils;
+import com.example.veryw.testveryssssss.view.CustomGallery;
+
+public class GalleryActivity extends BaseActivity {
+    private int[] imageResIDs;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+
+        CustomGallery mGallery = (CustomGallery) findViewById(R.id.customgallery);
+
+        imageResIDs = new int[] {
+                R.drawable.pic_1,
+                R.drawable.pic_2,
+                R.drawable.pic_3,
+                R.drawable.pic_4,
+                R.drawable.pic_5,
+                R.drawable.pic_6,
+                R.drawable.pic_7,
+                R.drawable.pic_8,
+                R.drawable.pic_1,
+                R.drawable.pic_2,
+                R.drawable.pic_3,
+                R.drawable.pic_4,
+                R.drawable.pic_5,
+                R.drawable.pic_6,
+                R.drawable.pic_7,
+                R.drawable.pic_8,
+                R.drawable.pic_1,
+                R.drawable.pic_2,
+                R.drawable.pic_3,
+                R.drawable.pic_4,
+                R.drawable.pic_5,
+                R.drawable.pic_6,
+                R.drawable.pic_7,
+                R.drawable.pic_8,
+                R.drawable.pic_1,
+                R.drawable.pic_2,
+                R.drawable.pic_3,
+                R.drawable.pic_4,
+                R.drawable.pic_5,
+                R.drawable.pic_6,
+                R.drawable.pic_7,
+                R.drawable.pic_8,
+                R.drawable.pic_1,
+                R.drawable.pic_2,
+                R.drawable.pic_3,
+                R.drawable.pic_4,
+                R.drawable.pic_5,
+                R.drawable.pic_6,
+                R.drawable.pic_7,
+                R.drawable.pic_8,
+                R.drawable.pic_1,
+                R.drawable.pic_2,
+                R.drawable.pic_3,
+                R.drawable.pic_4,
+                R.drawable.pic_5,
+                R.drawable.pic_6,
+                R.drawable.pic_7,
+                R.drawable.pic_8,
+                R.drawable.pic_1,
+                R.drawable.pic_2,
+                R.drawable.pic_3,
+                R.drawable.pic_4,
+                R.drawable.pic_5,
+                R.drawable.pic_6,
+                R.drawable.pic_7,
+                R.drawable.pic_8,
+                R.drawable.pic_1,
+                R.drawable.pic_2,
+                R.drawable.pic_3,
+                R.drawable.pic_4,
+                R.drawable.pic_5,
+                R.drawable.pic_6,
+                R.drawable.pic_7,
+                R.drawable.pic_8,
+                R.drawable.pic_1,
+                R.drawable.pic_2,
+                R.drawable.pic_3,
+                R.drawable.pic_4,
+                R.drawable.pic_5,
+                R.drawable.pic_6,
+                R.drawable.pic_7,
+                R.drawable.pic_8,
+                R.drawable.pic_1,
+                R.drawable.pic_2,
+                R.drawable.pic_3,
+                R.drawable.pic_4,
+                R.drawable.pic_5,
+                R.drawable.pic_6,
+                R.drawable.pic_7,
+                R.drawable.pic_8
+        };
+
+        mGallery.setAdapter(new MyAdapter());
+    }
+
+
+    class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return imageResIDs.length;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView iv = null;
+            if(convertView == null) {
+                iv = new ImageView(GalleryActivity.this);
+            } else {
+                iv = (ImageView) convertView;
+            }
+
+            Bitmap bitmap = ImageUtils.getImageBitmap(
+                    GalleryActivity.this.getResources(), imageResIDs[position]);
+
+            BitmapDrawable bd = new BitmapDrawable(bitmap);
+            bd.setAntiAlias(true);		// 消除锯齿
+
+            iv.setImageDrawable(bd);
+            Gallery.LayoutParams params = new Gallery.LayoutParams(200, 260);
+            iv.setLayoutParams(params);
+            return iv;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+
+    }
+}
