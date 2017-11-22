5b269f0e151b6698ad4f5f82ba0c630e364ffea5
soshuai <601647787@qq.com>
2017-11-22 10:05:30 +0800

    添加3d画廊

--- app/src/main/java/com/example/veryw/testveryssssss/MainActivity.java
+++ app/src/main/java/com/example/veryw/testveryssssss/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.veryw.testveryssssss;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.LinearLayout;
+
 import com.example.veryw.testveryssssss.adapter.MyAdapter;
 import com.example.veryw.testveryssssss.base.BaseActivity;
 
@@ -42,6 +44,7 @@ public class MainActivity extends BaseActivity implements MyAdapter.Recyclerview
         }
         strings[0] = "温水煮青蛙";
         strings[1] = "Rotate旋转动画";
+        strings[2] = "3D画廊";
         return strings;
     }
 
@@ -49,12 +52,26 @@ public class MainActivity extends BaseActivity implements MyAdapter.Recyclerview
     public void onclickHaha(View view, int position) {
         switch (position) {
             case 1:
-                startActivity(new Intent(this,RotateActivity.class));
+                Intent intent1=new Intent(this, RotateActivity.class);
+                startActivity(intent1);
                 break;
-
+            case 2:
+                Intent intent2=new Intent(this, GalleryActivity.class);
+                startActivity(intent2);
             default:
                 break;
         }
-        androidToast("点击了" + strings[position]);
+
+        toast(position);
+
+    }
+
+    private void toast(int position) {
+        if (position == 0) {
+            androidToast("呱呱呱。。。");
+            return;
+        }
+        if (strings[position].contains("我的第"))
+            androidToast("点击了" + strings[position]);
     }
 }
