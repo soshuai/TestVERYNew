cdd479a676e57d2b3f84f3cc20984dcec21fb647
soshuai <601647787@qq.com>
2018-04-04 18:34:46 +0800

    111111111111111

---
+++ app/src/main/java/com/example/veryw/testveryssssss/GuolinListActivity.java
@@ -0,0 +1,34 @@
+package com.example.veryw.testveryssssss;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.example.veryw.testveryssssss.base.BaseActivity;
+import com.example.veryw.testveryssssss.view.RefreshableView;
+
+public class GuolinListActivity extends BaseActivity {
+    RefreshableView refreshableView;
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    String[] items = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L" };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guolin_list);
+        refreshableView = (RefreshableView) findViewById(R.id.refreshable_view);
+        listView = (ListView) findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+        refreshableView.setOnRefreshListener(new RefreshableView.PullToRefreshListener() {
+            @Override
+            public void onRefresh() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                refreshableView.finishRefreshing();
+            }
+        }, 0);
+    }
+}
